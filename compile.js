0239153a0f213b027510364270295e3db726c631
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-04-21 21:04:13 +0200

    typo

--- compile.js
+++ compile.js
@@ -5,7 +5,6 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-
 const input = {
   language: 'Solidity',
   sources: {
@@ -21,5 +20,9 @@ const input = {
     },
   },
 };
-// The output is structured differntly than in old Solidity version, hence the change :
+
+// console.log(JSON.parse(solc.compile(JSON.stringify(input))));
+
+
+// The output is structured differently than in old Solidity version, hence the change :
 module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Inbox.sol'].Inbox;
